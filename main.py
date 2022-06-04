11c022418fe0e27eb6e83c6f7905474f04dca297
Daniyal Bokhari <63890998+daniyalb@users.noreply.github.com>
2022-06-04 01:21:03 -0400

    Added movement animations
    Added movement animations to show character legs moving when a movement key is pressed. The sprites change to show different legs moving back and forth to simulate walking.

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ WIDTH, HEIGHT = 700, 700
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 ICON = pygame.image.load('Assets/icon.png')
 BACKGROUND = pygame.image.load('Assets/background.jpg')
-CHARACTER = pygame.image.load('Assets/char_still.png')
+CHARACTER_STILL = pygame.image.load('Assets/char_still.png')
+CHARACTER_RIGHT = pygame.image.load('Assets/char_right.png')
+CHARACTER_LEFT = pygame.image.load('Assets/char_left.png')
 CHAR_DIM = 128
 SPEED = 3
 pygame.display.set_caption("Moon Mayhem 2")
@@ -21,35 +23,62 @@ class Player():
 
     === Public Attributes ==
     sprite:
-        The sprite surface for the character
+         The sprite surface for the character
     character:
-        The pygame rectangle representing the character
+         The pygame rectangle representing the character
     """
+    # === Private Attributes ==
+    # _move_sprites:
+    #     a list of the two image surfaces that represent the character moving
+    # _curr_move_sprite
+    #     An index number for which movement sprite is currently selected
     sprite: pygame.Surface
     rect: pygame.Rect
+    last_updated: int
 
     def __init__(self, CHARACTER: pygame.Surface) -> None:
-        self.rect = pygame.Rect(WIDTH // 2 - CHAR_DIM // 2,  # astro
-                        HEIGHT // 2 - CHAR_DIM // 2, CHAR_DIM, CHAR_DIM)
-        self.sprite = CHARACTER
+        self.sprite = CHARACTER_STILL
+        self._move_sprites = [CHARACTER_RIGHT, CHARACTER_LEFT]
+        self._curr_move_sprite = 0
+        self.rect = self.sprite.get_rect()
+        self.rect.center = (WIDTH // 2, HEIGHT // 2)
+        self._last_updated = 0
     
     def move(self, key_pressed) -> None:
         """Move the character depending on which keys are pressed in key_pressed
         """
+        moving = False
         if key_pressed[pygame.K_a] and self.rect.left >= 0:
-            self.rect.x -= SPEED
+            self.rect.centerx -= SPEED
+            moving = True
         if key_pressed[pygame.K_d] and self.rect.right <= WIDTH:
-            self.rect.x += SPEED
+            self.rect.centerx += SPEED
+            moving = True
         if key_pressed[pygame.K_w] and self.rect.top >= 0:
-            self.rect.y -= SPEED
+            self.rect.centery -= SPEED
+            moving = True
         if key_pressed[pygame.K_s] and self.rect.bottom <= HEIGHT:
-            self.rect.y += SPEED
+            self.rect.centery += SPEED
+            moving = True
+        if moving:
+            self.animate()
+        else:
+            self.sprite = CHARACTER_STILL
+    
+    def animate(self) -> None:
+        """Animate the character by moving its legs when the character is moving"""
+        now = pygame.time.get_ticks()
+        if now - self._last_updated > 200:
+            self._last_updated = now
+            self._curr_move_sprite = (self._curr_move_sprite + 1) % len(self._move_sprites)
+            self.sprite = self._move_sprites[self._curr_move_sprite]
+
 
     def get_rotated(self, rotation: float) -> pygame.Surface:
         """Rotate the character surface acording to the <rotation> value and return
         this surface
         """
-        return pygame.transform.rotate(pygame.transform.scale(CHARACTER, (CHAR_DIM,
+        return pygame.transform.rotate(pygame.transform.scale(self.sprite, (CHAR_DIM,
                                         CHAR_DIM)), rotation)
 
 
@@ -63,7 +92,7 @@ def draw_window(char: Player, rotation: float):
 def main():
     run = True
     clock = pygame.time.Clock()
-    char = Player(CHARACTER)
+    char = Player(CHARACTER_STILL)
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
