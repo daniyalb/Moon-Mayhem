70dab9d2005f442d1c6c1497c59389124d021135
Daniyal Bokhari <63890998+daniyalb@users.noreply.github.com>
2022-06-17 00:04:59 -0400

    Enemy Class made abstract/Added health regen
    Moved the Enemy class to a separate file to make it easier to modify code in different sections of the main file.
    Made Enemy class an abstract class and created a subclass Snake() for the current enemy in the game.
    Added health regeneration for the player.

---
+++ enemy.py
@@ -0,0 +1,328 @@
+import pygame
+import random
+
+pygame.init()
+WIDTH, HEIGHT = 1280, 720
+ENEMY_HIT = pygame.mixer.Sound('Assets/sounds/enemy_hit.wav')
+ENEMY_DEATH = pygame.mixer.Sound('Assets/sounds/enemy_death.wav')
+ENEMY_SPEED = 1
+GREEN = (0, 128, 0)
+RED = (255, 0, 0)
+
+
+class Enemy:
+    """ The enemy class for this game.
+
+    Do not instantiate this class, it is an abstract class.
+
+    === Public Attributes ===
+    rect:
+         The pygame rectangle for this enemy
+    curr_sprite:
+         The sprite which is currently selected and is being displayed on the
+         screen
+    health:
+         The amount of health, or lives, this enemy has
+    play_hit_animation:
+         A boolean variable that is checked to see if the enemy hit animation
+         should be played
+    dead:
+         A boolean indicating whether the enemy is dead (True) or alive (false)
+    remove:
+         A boolean indicating whether this enemy should be removed from the
+         enemy list in the main() function
+    """
+    # === Private Attributes ===
+    # _enemy_sprites:
+    #     A list containing the various sprites of the enemies
+    # _facing:
+    #     A string that represents the direction this enemy's sprite is
+    #     currently facing
+    # _last_updated:
+    #     An integer mean to keep track of the frame when this enemy's
+    #     animation was last updated
+    # _curr_frame_sprite:
+    #     An integer meant to signify the index of which sprite in
+    #     <_enemy_sprites> is selected
+    # _hit_sprites:
+    #     A list of sprites representing the blood when this enemy is hot
+    # _death_sprites:
+    #    A list of sprites representing the blood when this enemy dies
+    # _last_update_hit:
+    #     An integer mean to keep track of the frame when this enemy's
+    #     hit animation was last updated
+    # _last_update_dead:
+    #     An integer mean to keep track of the frame when this enemy's
+    #     death animation was last updated
+    # _curr_hit_sprite:
+    #     An integer meant to signify the index of which sprite in
+    #     <_hit_sprites> is selected
+    # _curr_death_sprite:
+    #     An integer meant to signify the index of which sprite in
+    #     <_death_sprites> is selected
+
+    rect: pygame.Rect
+    curr_sprite: pygame.Surface
+    health: int
+    play_hit_animation: bool
+    hit_sprites: list[pygame.Surface]
+    dead: bool
+    remove: bool
+    _enemy_sprites: list
+    _facing: str
+    _last_updated: int
+    _curr_frame_sprite: int
+    _curr_hit_sprite: int
+    _curr_death_sprite: int
+    _death_sprites: list[pygame.Surface]
+    _last_update_hit: int
+    _last_update_dead: int
+
+    def __init__(self) -> None:
+        self._last_updated = 0
+        self._curr_frame_sprite = 0
+        self._last_update_hit = 0
+        self._last_update_dead = 0
+        self._curr_hit_sprite = 0
+        self._curr_death_sprite = 0
+        self.health = 3
+        self._facing = 'right'
+        self.dead = False
+        self.remove = False
+        self._init_sprites()
+        self._get_hit_sprites()
+        self._get_death_sprites()
+        self.curr_sprite = self._enemy_sprites[0]
+        self.rect = self.curr_sprite.get_rect()
+        self.play_hit_animation = False
+        self.spawn()
+
+    def _get_hit_sprites(self) -> None:
+        """ Initialize the sprites representing the blood when the enemy gets
+        hit as pygame.Surface and place them into a list
+        <self._hit_sprites>
+        """
+        raise NotImplementedError
+
+    def _init_sprites(self) -> None:
+        """ Initialize the sprite with all of its animations into a list
+        <self._enemy_sprites>
+        """
+        raise NotImplementedError
+
+    def _get_death_sprites(self) -> None:
+        """ Initialize the sprites representing the blood when the enemy dies
+        as pygame.Surface and place them into a list <self._death_sprites>
+        """
+        raise NotImplementedError
+
+    def spawn(self) -> None:
+        """ Spawn in this enemy somewhere outside of the viewable screen
+        """
+        x, y = 0, 0
+        while (0 <= x <= WIDTH) and (0 <= y <= HEIGHT):
+            x = random.randint(-500, WIDTH + 500)
+            y = random.randint(-500, HEIGHT + 500)
+        self.rect.center = (x, y)
+
+    def move(self, x: int, y: int) -> None:
+        if self.rect.x <= x:
+            self.rect.x += ENEMY_SPEED
+        else:
+            self.rect.x -= ENEMY_SPEED
+
+        if self.rect.y <= y:
+            self.rect.y += ENEMY_SPEED
+        else:
+            self.rect.y -= ENEMY_SPEED
+
+    def damage(self) -> None:
+        """ Register the fact that this enemy was damaged
+        """
+        self.health -= 1
+        ENEMY_HIT.play()
+        if self.health == 0:
+            self.dead = True
+            ENEMY_DEATH.play()
+        else:
+            self.play_hit_animation = True
+
+    def draw_health(self, window: pygame.Surface) -> None:
+        x1, y1 = self.rect.bottomleft
+        x1 += 16
+        y1 += 6
+        x2, y2 = self.rect.bottomright
+        x2 -= 16
+        y2 += 6
+
+        if self.health == 2:
+            split = int((x2 - x1) * 0.66)
+            split += x1
+            pygame.draw.line(window, GREEN, (x1, y1), (split, y2), 4)
+            pygame.draw.line(window, RED, (split, y1), (x2, y2), 4)
+        elif self.health == 1:
+            split = int((x2 - x1) * 0.33)
+            split += x1
+            pygame.draw.line(window, GREEN, (x1, y1), (split, y2), 4)
+            pygame.draw.line(window, RED, (split, y1), (x2, y2), 4)
+        else:
+            pygame.draw.line(window, GREEN, (x1, y1), (x2, y2), 4)
+
+    def animate(self, x: int, y: int) -> None:
+        """ Animate the enemy by cycling through it's animation as it moves
+        """
+        now = pygame.time.get_ticks()
+
+        if now - self._last_updated > 200:
+            self._last_updated = now
+            self._curr_frame_sprite = (self._curr_frame_sprite + 1) % len(
+                self._enemy_sprites)
+            if self._facing == 'right':
+                self.curr_sprite = self._enemy_sprites[self._curr_frame_sprite]
+            else:
+                self.curr_sprite = pygame.transform.flip(self._enemy_sprites[
+                                        self._curr_frame_sprite], True, False)
+
+            if x < self.rect.x and self._facing == 'right':
+                self.curr_sprite = pygame.transform.flip(self.curr_sprite, True,
+                                                         False)
+                self._facing = 'left'
+            elif x > self.rect.x and self._facing == 'left':
+                self.curr_sprite = pygame.transform.flip(self.curr_sprite, True,
+                                                         False)
+                self._facing = 'right'
+
+
+class Snake(Enemy):
+    """The Enemy class for this game.
+
+    This class handles the drawing of, spawning in, and health and damage
+    caused by the enemies.
+
+    === Public Attributes ===
+    rect:
+         The pygame rectangle for this enemy
+    curr_sprite:
+         The sprite which is currently selected and is being displayed on the
+         screen
+    health:
+         The amount of health, or lives, this enemy has
+    play_hit_animation:
+         A boolean variable that is checked to see if the enemy hit animation
+         should be played
+    dead:
+         A boolean indicating whether the enemy is dead (True) or alive (false)
+    remove:
+         A boolean indicating whether this enemy should be removed from the
+         enemy list in the main() function
+    """
+    # === Private Attributes ===
+    # _enemy_sprites:
+    #     A list containing the various sprites of the enemies
+    # _facing:
+    #     A string that represents the direction this enemy's sprite is
+    #     currently facing
+    # _last_updated:
+    #     An integer mean to keep track of the frame when this enemy's
+    #     animation was last updated
+    # _curr_frame_sprite:
+    #     An integer meant to signify the index of which sprite in
+    #     <_enemy_sprites> is selected
+    # _hit_sprites:
+    #     A list of sprites representing the blood when this enemy is hot
+    # _death_sprites:
+    #    A list of sprites representing the blood when this enemy dies
+    # _last_update_hit:
+    #     An integer mean to keep track of the frame when this enemy's
+    #     hit animation was last updated
+    # _last_update_dead:
+    #     An integer mean to keep track of the frame when this enemy's
+    #     death animation was last updated
+    # _curr_hit_sprite:
+    #     An integer meant to signify the index of which sprite in
+    #     <_hit_sprites> is selected
+    # _curr_death_sprite:
+    #     An integer meant to signify the index of which sprite in
+    #     <_death_sprites> is selected
+
+    rect: pygame.Rect
+    curr_sprite: pygame.Surface
+    health: int
+    play_hit_animation: bool
+    hit_sprites: list[pygame.Surface]
+    dead: bool
+    remove: bool
+    _enemy_sprites: list
+    _facing: str
+    _last_updated: int
+    _curr_frame_sprite: int
+    _curr_hit_sprite: int
+    _curr_death_sprite: int
+    _death_sprites: list[pygame.Surface]
+    _last_update_hit: int
+    _last_update_dead: int
+
+    def _get_hit_sprites(self) -> None:
+        """ Initialize the sprites representing the blood when the enemy gets
+        hit as pygame.Surface and place them into a list
+        <self._hit_sprites>
+        """
+        self.hit_sprites = []
+        for i in range(1, 29):
+            sprite = pygame.image.load('Assets/enemies/enemy_hit/' + str(i) +
+                                       '.png')
+            sprite = pygame.transform.scale(sprite, (300, 300))
+            self.hit_sprites.append(sprite)
+
+    def _init_sprites(self) -> None:
+        """ Initialize the sprite with all of its animations into a list
+        <self._enemy_sprites>
+        """
+        self._enemy_sprites = []
+        for i in range(1, 10):
+            sprite = pygame.image.load('Assets/enemies/worm' + str(i) + '.png')
+            sprite = pygame.transform.scale(sprite, (100, 100))
+            self._enemy_sprites.append(sprite)
+
+    def _get_death_sprites(self) -> None:
+        """ Initialize the sprites representing the blood when the enemy dies
+        as pygame.Surface and place them into a list <self._death_sprites>
+        """
+        self._death_sprites = []
+        for i in range(1, 31):
+            sprite = pygame.image.load('Assets/enemies/enemy_death/' + str(i) +
+                                       '.png')
+            sprite = pygame.transform.scale(sprite, (300, 300))
+            self._death_sprites.append(sprite)
+
+    def hit_animation(self, window: pygame.Surface) -> None:
+        now = pygame.time.get_ticks()
+
+        if now - self._last_update_hit > 25:
+            self._last_update_hit = now
+            self._curr_hit_sprite = (self._curr_hit_sprite + 1) % len(
+                self.hit_sprites)
+
+        x, y = self.rect.topleft
+        x -= 80
+        y -= 80
+        window.blit(self.hit_sprites[self._curr_hit_sprite], (x, y))
+
+        if self._curr_hit_sprite == 27:
+            self.play_hit_animation = False
+
+    def death_animation(self, window: pygame.Surface) -> None:
+        now = pygame.time.get_ticks()
+
+        if now - self._last_update_dead > 25:
+            self._last_update_dead = now
+            self._curr_death_sprite = (self._curr_death_sprite + 1) % len(
+                self._death_sprites)
+
+        x, y = self.rect.topleft
+        x -= 60
+        y -= 60
+        window.blit(self._death_sprites[self._curr_death_sprite], (x, y))
+
+        if self._curr_death_sprite == 29:
+            self.remove = True
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from lib2to3.pygram import python_grammar_no_print_statement
-import py
 import pygame
 import math
-import random
+from enemy import Snake
 
 pygame.init()
 WIDTH, HEIGHT = 1280, 720
@@ -12,11 +10,12 @@ BACKGROUND = pygame.image.load('Assets/misc/background.jpg')
 BAR = pygame.image.load('Assets/misc/bar.png')
 BAR = pygame.transform.scale(BAR, (WIDTH, 50))
 LASER = pygame.image.load('Assets/misc/laser.png')
-HEART = pygame.transform.scale(pygame.image.load('Assets/misc/heart.png'), (32, 32))
+HEALTH = pygame.transform.scale(pygame.image.load('Assets/misc/health_title.png'
+                                                  ), (113, 23))
+HEART = pygame.transform.scale(pygame.image.load('Assets/misc/heart.png'), (32,
+                                                                            32))
 LASER_SOUND = pygame.mixer.Sound('Assets/sounds/laser_sound.wav')
 LASER_SOUND.set_volume(0.2)
-ENEMY_HIT = pygame.mixer.Sound('Assets/sounds/enemy_hit.wav')
-ENEMY_DEATH = pygame.mixer.Sound('Assets/sounds/enemy_death.wav')
 CHARACTER_STILL = pygame.image.load('Assets/character/char_still.png')
 CHARACTER_RIGHT = pygame.image.load('Assets/character/char_right.png')
 CHARACTER_LEFT = pygame.image.load('Assets/character/char_left.png')
@@ -24,16 +23,13 @@ CHAR_HIT = pygame.mixer.Sound('Assets/sounds/player_hit.wav')
 CHAR_DIM = 128
 SPEED = 5
 BULL_VEL = 6
-ENEMY_SPEED = 1
 BLACK = (0, 0, 0)
-GREEN = (0, 128, 0)
-RED = (255, 0, 0)
 pygame.display.set_caption("Moon Mayhem 2")
 pygame.display.set_icon(ICON)
 FPS = 60
 
 
-class Player():
+class Player:
     """The player character in the game.
 
     This class handles the drawing and movement of the character in the game, as
@@ -62,6 +58,9 @@ class Player():
     # _last_update_damage:
     #     An integer that tracks when the last time collisions were detected
     #     between the player and enemies
+    # _last_update_health:
+    #     An integer that tracks when the last time player health checked and
+    #     increased if it was less than 5
 
     sprite: pygame.Surface
     rect: pygame.Rect
@@ -72,6 +71,7 @@ class Player():
     _death_sprites: list[pygame.Surface]
     _last_updated: int
     _last_update_damage: int
+    _last_update_health: int
 
     def __init__(self) -> None:
         self.sprite = CHARACTER_STILL
@@ -82,6 +82,7 @@ class Player():
         self.rect.center = (WIDTH // 2, HEIGHT // 2)
         self._last_updated = 0
         self._last_update_damage = 0
+        self._last_update_health = 0
         self.bullets = []
         self.health = 5
 
@@ -117,17 +118,18 @@ class Player():
                 self._move_sprites)
             self.sprite = self._move_sprites[self._curr_move_sprite]
 
-    def _get_death_sprites(self) -> None:   # TODO: Implement player death
+    def _get_death_sprites(self) -> None:
         """ Load in the sprites representing blood when the character dies
         and place them all into a list <self._death_sprties>
         """
         self._death_sprites = []
         for i in range(1, 23):
-            sprite = pygame.image.load('Assets/character/character_hit/' + str(i) + '.png')
+            sprite = pygame.image.load('Assets/character/character_hit/' + str(
+                i) + '.png')
             self._death_sprites.append(sprite)
 
     def get_rotated(self, rotation: float) -> pygame.Surface:
-        """Rotate the character surface acording to the <rotation> value and
+        """Rotate the character surface according to the <rotation> value and
         return
         this surface
         """
@@ -144,11 +146,12 @@ class Player():
         LASER_SOUND.play()
         self.bullets.append((bullet, x_vel, y_vel))
 
-    def _handle_bullets(self, bullet: tuple[pygame.Rect, int, int], 
+    def _handle_bullets(self, bullet: tuple[pygame.Rect, float, float],
                         enemies: list) -> None:
         """Handle bullet collisions
         """
-        if bullet[0].x <= 0 or bullet[0].x >= WIDTH or bullet[0].y <= 0 or bullet[0].y >= HEIGHT:
+        if bullet[0].x <= 0 or bullet[0].x >= WIDTH or bullet[0].y <= 0 or \
+                bullet[0].y >= HEIGHT:
             self.bullets.remove(bullet)
         for enemy in enemies:
             if enemy.rect.colliderect(bullet[0]) and not enemy.dead:
@@ -182,230 +185,21 @@ class Player():
                 CHAR_HIT.play()
             self._last_update_damage = now
 
-
-class Enemy:
-    """The Enemy class for this game.
-
-    This class handles the drawing of, spawning in, and health and damage
-    caused by the enemies.
-
-    === Public Attributes ===
-    rect:
-         The pygame rectangle for this enemy
-    curr_sprite:
-         The sprite which is currently selected and is being displayed on the screen
-    health:
-         The amount of health, or lives, this enemy has
-    play_hit_animation:
-         A boolean variable that is checked to see if the enemy hit animation
-         should be played
-    dead:
-         A boolean indicating whether the enemy is dead (True) or alive (false)
-    remove:
-         A boolean indicating whether this enemy should be removed from the enemy
-         list in the main() function
-    """
-    # === Private Attributes ===
-    # _enemy_sprites:
-    #     A list containing the various sprites of the enemies
-    # _facing:
-    #     A string that represents the direction this enemy's sprite is currently facing
-    # _last_updated:
-    #     An integer mean to keep track of the frame when this enemy's
-    #     animation was last updated
-    # _curr_frame_sprite:
-    #     An integer meant to signify the index of which sprite in <_enemy_sprites>
-    #     is selected
-    # _hit_sprites:
-    #     A list of sprites representing the blood when this enemy is hot
-    # _death_sprites:
-    #    A list of sprites representing the blood when this enemy dies
-    # _last_update_hit:
-    #     An integer mean to keep track of the frame when this enemy's
-    #     hit animation was last updated
-    # _last_update_dead:
-    #     An integer mean to keep track of the frame when this enemy's
-    #     death animation was last updated
-    # _curr_hit_sprite:
-    #     An integer meant to signify the index of which sprite in <_hit_sprites>
-    #     is selected
-    # _curr_death_sprite:
-    #     An integer meant to signify the index of which sprite in <_death_sprites>
-    #     is selected
-
-    rect: pygame.Rect
-    curr_sprite: pygame.Surface
-    health: int
-    play_hit_animation: bool
-    hit_sprites: list[pygame.Surface]
-    dead: bool
-    remove: bool
-    _enemy_sprites: list
-    _facing: str
-    _last_updated: int
-    _curr_frame_sprite: int
-    _curr_hit_sprite: int
-    _curr_death_sprite: int
-    _death_sprites: list[pygame.Surface]
-    _last_update_hit: int
-    _last_update_dead: int
-
-    def __init__(self) -> None:
-        self._last_updated = 0
-        self._curr_frame_sprite = 0
-        self._last_update_hit = 0
-        self._last_update_dead = 0
-        self._curr_hit_sprite = 0
-        self._curr_death_sprite = 0
-        self.health = 3
-        self._facing = 'right'
-        self.dead = False
-        self.remove = False
-        self._init_sprites()
-        self._get_hit_sprites()
-        self._get_death_sprites()
-        self.curr_sprite = self._enemy_sprites[0]
-        self.rect = self.curr_sprite.get_rect()
-        self.play_hit_animation = False
-        self.spawn()
-
-    def _get_hit_sprites(self) -> None:
-        """ Initialize the sprites representing the blood when the enemy gets
-        hit as pygame.Surface and place them into a list 
-        <self._hit_sprites>
-        """
-        self.hit_sprites = []
-        for i in range(1, 29):
-            sprite = pygame.image.load('Assets/enemies/enemy_hit/' + str(i) + '.png')
-            sprite = pygame.transform.scale(sprite, (300, 300))
-            self.hit_sprites.append(sprite)
-
-    def _init_sprites(self) -> None:
-        """ Initialize the sprite with all of its animations into a list 
-        <self._enemy_sprites>
-        """
-        self._enemy_sprites = []
-        for i in range(1, 10):
-            sprite = pygame.image.load('Assets/enemies/worm' + str(i) + '.png')
-            sprite = pygame.transform.scale(sprite, (100, 100))
-            self._enemy_sprites.append(sprite)
-
-    def _get_death_sprites(self) -> None:
-        """ Initialize the sprites representing the blood when the enemy dies
-        as pygame.Surface and place them into a list <self._death_sprites>
-        """
-        self._death_sprites = []
-        for i in range(1, 31):
-            sprite = pygame.image.load('Assets/enemies/enemy_death/' + str(i) + '.png')
-            sprite = pygame.transform.scale(sprite, (300, 300))
-            self._death_sprites.append(sprite)
-
-    def spawn(self) -> None:
-        """ Spawn in this enemy somewhere outside of the viewable screen
-        """
-        x, y = 0, 0
-        while (x >= 0 and x <= WIDTH) and (y >= 0 and y <= HEIGHT):
-            x = random.randint(-500, WIDTH + 500)
-            y = random.randint(-500, HEIGHT + 500)
-        self.rect.center = (x, y)
-
-    def move(self, x: int, y: int) -> None:
-        if self.rect.x <= x:
-            self.rect.x += ENEMY_SPEED
-        else:
-            self.rect.x -= ENEMY_SPEED
-
-        if self.rect.y <= y:
-            self.rect.y += ENEMY_SPEED
-        else:
-            self.rect.y -= ENEMY_SPEED
-
-    def damage(self) -> None:
-        """ Register the fact that this enemy was damaged
-        """
-        self.health -= 1
-        ENEMY_HIT.play()
-        if self.health == 0:
-            self.dead = True
-            ENEMY_DEATH.play()
-        else:
-            self.play_hit_animation = True
-
-    def hit_animation(self, window: pygame.Surface) -> None:
-        now = pygame.time.get_ticks()
-
-        if now - self._last_update_hit > 25:
-            self._last_update_hit = now
-            self._curr_hit_sprite = (self._curr_hit_sprite + 1) % len(self.hit_sprites)
-
-        x, y = self.rect.topleft
-        x -= 80
-        y -= 80
-        window.blit(self.hit_sprites[self._curr_hit_sprite], (x, y))
-        
-        if self._curr_hit_sprite == 27:
-            self.play_hit_animation = False
-
-    def death_animation(self, window: pygame.Surface) -> None:
-        now = pygame.time.get_ticks()
-
-        if now - self._last_update_dead > 25:
-            self._last_update_dead = now
-            self._curr_death_sprite = (self._curr_death_sprite + 1) % len(self._death_sprites)
-
-        x, y = self.rect.topleft
-        x -= 60
-        y -= 60
-        window.blit(self._death_sprites[self._curr_death_sprite], (x, y))
-        
-        if self._curr_death_sprite == 29:
-            self.remove = True
-
-    def draw_health(self, window: pygame.Surface) -> None:
-        x1, y1 = self.rect.bottomleft
-        x1 += 16
-        y1 += 6
-        x2, y2 = self.rect.bottomright
-        x2 -= 16
-        y2 += 6
-
-        if self.health == 2:
-            split = int((x2 - x1) * 0.66)
-            split += x1
-            pygame.draw.line(window, GREEN, (x1, y1), (split, y2), 4)
-            pygame.draw.line(window, RED, (split, y1), (x2, y2), 4)
-        elif self.health == 1:
-            split = int((x2 - x1) * 0.33)
-            split += x1
-            pygame.draw.line(window, GREEN, (x1, y1), (split, y2), 4)
-            pygame.draw.line(window, RED, (split, y1), (x2, y2), 4)
-        else:
-            pygame.draw.line(window, GREEN, (x1, y1), (x2, y2), 4)
-
-    def animate(self, x: int, y: int) -> None:
-        """ Animate the enemy by cycling through it's animation as it moves
+    def update_health(self) -> None:
+        """ Use a clock to determine when the player's health should be increased
+        and only increase health if self.health < 5
         """
         now = pygame.time.get_ticks()
-
-        if now - self._last_updated > 200:
-            self._last_updated = now
-            self._curr_frame_sprite = (self._curr_frame_sprite + 1) % len(self._enemy_sprites)
-            if self._facing == 'right':
-                self.curr_sprite = self._enemy_sprites[self._curr_frame_sprite]
-            else:
-                self.curr_sprite = pygame.transform.flip(self._enemy_sprites[self._curr_frame_sprite], True, False)
-                
-            if x < self.rect.x and self._facing == 'right':
-                self.curr_sprite = pygame.transform.flip(self.curr_sprite, True, False)
-                self._facing = 'left'
-            elif x > self.rect.x and self._facing == 'left':
-                self.curr_sprite = pygame.transform.flip(self.curr_sprite, True, False)
-                self._facing = 'right'
+        if now - self._last_update_health > 2000:
+            self._last_update_health = now
+            if self.health < 5:
+                self.health += 1
 
 
 def draw_window(char: Player, rotation: float, enemies: list):
     WINDOW.blit(BACKGROUND, (0, 0))
     WINDOW.blit(BAR, (0, HEIGHT - 50))
+    WINDOW.blit(HEALTH, (WIDTH - 370, HEIGHT - 40))
     curr_char = char.get_rotated(rotation)
     char.draw_bullets(WINDOW, enemies)
     WINDOW.blit(curr_char, (char.rect.x, char.rect.y))
@@ -425,7 +219,7 @@ def main():
     run = True
     clock = pygame.time.Clock()
     char = Player()
-    enemies = [Enemy()]
+    enemies = [Snake()]
     pygame.mixer.music.load('Assets/sounds/background_music.wav')
     pygame.mixer.music.set_volume(0.6)
     pygame.mixer.music.play(-1)
@@ -443,6 +237,7 @@ def main():
 
         key_pressed = pygame.key.get_pressed()
         char.move(key_pressed)
+        char.update_health()
 
         for enemy in enemies:
             if enemy.remove:
@@ -451,7 +246,7 @@ def main():
                 enemy.animate(char.rect.x, char.rect.y)
                 enemy.move(char.rect.x, char.rect.y)
                 char.check_damage(enemy)
-        
+
         draw_window(char, rotation, enemies)
     pygame.quit()
 
